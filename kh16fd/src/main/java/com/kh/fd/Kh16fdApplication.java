be60028d62860f5b0e7aebbc8e3960b891b9a7b6
hansoobin-11 <vcoart@naver.com>
2025-12-04 10:10:45 +0900

    최초 프로젝트 생성

---
+++ kh16fd/src/main/java/com/kh/fd/Kh16fdApplication.java
@@ -0,0 +1,13 @@
+package com.kh.fd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kh16fdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kh16fdApplication.class, args);
+	}
+
+}
---
+++ kh16fd/src/test/java/com/kh/fd/Kh16fdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.fd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kh16fdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
