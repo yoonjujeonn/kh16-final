723553723bea166586c0c07f56b00a8ef58c8292
charlie098' <charlie09811@gmail.com>
2025-12-08 12:15:38 +0900

    회원 정보 변경기능 완료

--- kh16fd/src/main/java/com/kh/fd/vo/MemberLoginResponseVO.java
+++ kh16fd/src/main/java/com/kh/fd/vo/MemberLoginResponseVO.java
@@ -9,8 +9,6 @@ import lombok.NoArgsConstructor;
 public class MemberLoginResponseVO {
 	private String loginId; //로그인한 사용자의 아이디
 	private String loginLevel; //로그인한 사용자의 등급
-	
-	//토큰은 당장 안씀
-//	private String accessToken; //나중에 사용자가 들고올 토큰
-//	private String refreshToken; //accessToken에 문제가 있을 때 갱신할 토큰
+	private String accessToken; //나중에 사용자가 들고올 토큰
+	private String refreshToken; //accessToken에 문제가 있을 때 갱신할 토큰
 }
