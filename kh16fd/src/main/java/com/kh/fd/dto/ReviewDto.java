ac55d0b41df2c1470c027139aac854b92bfbcb41
yoonjujeonn <mimi285611@gmail.com>
2025-12-10 15:43:35 +0900

    오류 수정

--- kh16fd/src/main/java/com/kh/fd/dto/ReviewDto.java
+++ kh16fd/src/main/java/com/kh/fd/dto/ReviewDto.java
@@ -2,6 +2,8 @@ package com.kh.fd.dto;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,6 +19,8 @@ public class ReviewDto {
     private String memberId;
     private String reviewContent;
     private double reviewRating;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
     private LocalDateTime reviewCreatedAt;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
     private LocalDateTime reviewUpdatedAt;
 }
