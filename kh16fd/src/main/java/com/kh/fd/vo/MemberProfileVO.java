73e3a6eaf8eae210dbdfad7acf478a4bbec6eced
charlie098' <charlie09811@gmail.com>
2025-12-16 17:26:01 +0900

    멤버 프로필 백엔드 구현 업로드와 갱신을 mapper에서 동시에

---
+++ kh16fd/src/main/java/com/kh/fd/vo/MemberProfileVO.java
@@ -0,0 +1,15 @@
+package com.kh.fd.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor @Builder
+public class MemberProfileVO {
+	private String memberId;
+	private int attachmentNo;
+	
+	private String profileName;
+	private String profileLink;
+}
