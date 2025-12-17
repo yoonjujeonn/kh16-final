8be4db29d0a738400258d00f289104ec59f012ca
kim9709 <kdk9709@naver.com>
2025-12-17 14:18:43 +0900

    카테고리 이미지 기능 수정(삭제 시 attachment에도 삭제+ 수정 시 그 항목 선택)

--- kh16fd/src/main/java/com/kh/fd/restcontroller/AdminRestController.java
+++ kh16fd/src/main/java/com/kh/fd/restcontroller/AdminRestController.java
@@ -6,19 +6,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 import com.kh.fd.dao.BannerDao;
 import com.kh.fd.dao.CategoryDao;
 import com.kh.fd.dao.RestaurantDao;
+import com.kh.fd.dao.ReviewDao;
 import com.kh.fd.dto.BannerDto;
 import com.kh.fd.dto.CategoryDto;
-import com.kh.fd.dao.ReviewDao;
 import com.kh.fd.dto.RestaurantDto;
 import com.kh.fd.error.TargetNotFoundException;
 import com.kh.fd.service.AttachmentService;
@@ -45,46 +39,53 @@ public class AdminRestController {
 
     @Autowired
     private AttachmentService attachmentService;
-    
-	@Autowired
-	private ReviewDao reviewDao;
 
+    @Autowired
+    private ReviewDao reviewDao;
 
-	// 승인 안된 식당 리스트
-		@GetMapping("/page/{page}")
-		public RestaurantApprovalListVO approvalList(@PathVariable int page) {
-			PageVO pageVO = new PageVO();
-			pageVO.setPage(page);
-			pageVO.setDataCount(restaurantDao.approvalCount());
+    // 승인 안된 식당 리스트
+    @GetMapping("/page/{page}")
+    public RestaurantApprovalListVO approvalList(@PathVariable int page) {
+        PageVO pageVO = new PageVO();
+        pageVO.setPage(page);
+        pageVO.setDataCount(restaurantDao.approvalCount());
 
-			List<RestaurantDto> list = restaurantDao.selectApprovalList(pageVO);
+        List<RestaurantDto> list = restaurantDao.selectApprovalList(pageVO);
 
-			return RestaurantApprovalListVO.builder().page(pageVO.getPage()).size(pageVO.getSize())
-					.count(pageVO.getDataCount()).begin(pageVO.getBegin()).end(pageVO.getEnd())
-					.last(pageVO.getPage() >= pageVO.getTotalPage()).list(list).build();
-		}
+        return RestaurantApprovalListVO.builder()
+                .page(pageVO.getPage())
+                .size(pageVO.getSize())
+                .count(pageVO.getDataCount())
+                .begin(pageVO.getBegin())
+                .end(pageVO.getEnd())
+                .last(pageVO.getPage() >= pageVO.getTotalPage())
+                .list(list)
+                .build();
+    }
 
-		// 상세 조회
-		@PostMapping("/{restaurantId}")
-		public RestaurantDto detail(@PathVariable long restaurantId) {
-			return restaurantDao.selectOne(restaurantId);
-		}
+    // 상세 조회
+    @PostMapping("/{restaurantId}")
+    public RestaurantDto detail(@PathVariable long restaurantId) {
+        return restaurantDao.selectOne(restaurantId);
+    }
 
-		// 관리자용 리뷰 전체 목록 조회
-		@GetMapping("/review/list")
-		public List<ReviewAdminVO> reviewList() {
-			return reviewDao.selectListAdmin();
-		}
+    // 관리자용 리뷰 전체 목록
+    @GetMapping("/review/list")
+    public List<ReviewAdminVO> reviewList() {
+        return reviewDao.selectListAdmin();
+    }
 
-		// 관리자용 리뷰 삭제
-		@DeleteMapping("/review/{reviewNo}")
-		public boolean deleteReview(@PathVariable int reviewNo) {
-			return reviewDao.delete(reviewNo);
-		}
+    // 관리자용 리뷰 삭제
+    @DeleteMapping("/review/{reviewNo}")
+    public boolean deleteReview(@PathVariable int reviewNo) {
+        return reviewDao.delete(reviewNo);
+    }
 
-    // 배너 관리
+    // 배너 등록
     @PostMapping("/banner")
-    public void addBanner(@ModelAttribute BannerDto bannerDto,@RequestParam(required = false) MultipartFile attach
+    public void addBanner(
+            @ModelAttribute BannerDto bannerDto,
+            @RequestParam(required = false) MultipartFile attach
     ) throws IOException {
 
         int bannerNo = bannerDao.sequence();
@@ -98,6 +99,7 @@ public class AdminRestController {
         bannerDao.insert(bannerDto);
     }
 
+    // 배너 삭제 
     @DeleteMapping("/banner/{bannerNo}")
     public void deleteBanner(@PathVariable int bannerNo) {
 
@@ -113,17 +115,19 @@ public class AdminRestController {
         }
     }
 
-    // 카테고리 관리
+    // 카테고리 등록
     @PostMapping("/category")
     public void addCategory(@RequestBody CategoryDto categoryDto) {
         int seq = (int) categoryDao.sequence();
         categoryDto.setCategoryNo(seq);
-
         categoryDao.insert(categoryDto);
     }
+
+    // 카테고리 수정
     @PutMapping("/category/{categoryNo}")
-    public void editCategory(@PathVariable int categoryNo,
-    		@RequestBody CategoryDto categoryDto
+    public void editCategory(
+            @PathVariable int categoryNo,
+            @RequestBody CategoryDto categoryDto
     ) {
         if (categoryDao.selectOne(categoryNo) == null) {
             throw new TargetNotFoundException("존재하지 않는 카테고리");
@@ -131,48 +135,52 @@ public class AdminRestController {
         categoryDto.setCategoryNo(categoryNo);
         categoryDao.updateUnit(categoryDto);
     }
-    @PatchMapping("/category/{categoryNo}")
-    public void editCategoryUnit(@PathVariable int categoryNo, 
-    		@RequestBody CategoryDto categoryDto
-    ) {
-        if (categoryDao.selectOne(categoryNo) == null) {
-            throw new TargetNotFoundException("존재하지 않는 카테고리");
-        }
 
-        categoryDto.setCategoryNo(categoryNo);
-        categoryDao.updateUnit(categoryDto);
-    }
-
+    // 카테고리 삭제
     @DeleteMapping("/category/{categoryNo}")
     public void deleteCategory(@PathVariable int categoryNo) {
         if (categoryDao.selectOne(categoryNo) == null) {
             throw new TargetNotFoundException("존재하지 않는 카테고리");
         }
-
         categoryDao.delete(categoryNo);
     }
 
-    // 카테고리 이미지 관리
+    // 카테고리 이미지 등록 
     @PostMapping("/category/image")
-    public void addCategoryImage(@RequestParam int categoryNo, @RequestParam MultipartFile attach
+    public void addCategoryImage(
+            @RequestParam int categoryNo,
+            @RequestParam MultipartFile attach
     ) throws IOException {
 
         if (categoryDao.selectOne(categoryNo) == null) {
             throw new TargetNotFoundException("존재하지 않는 카테고리");
         }
-        categoryDao.deleteCategoryImage(categoryNo);
+
+        Integer oldAttachmentNo = categoryDao.selectCategoryImageAttachmentNo(categoryNo);
+
+        if (oldAttachmentNo != null) {
+            attachmentService.delete(oldAttachmentNo);
+        }
+
         if (attach != null && !attach.isEmpty()) {
             int attachmentNo = attachmentService.save(attach);
             categoryDao.insertCategoryImage(categoryNo, attachmentNo);
         }
     }
+
+    //카테고리 이미지 삭제
     @DeleteMapping("/category/image/{categoryNo}")
     public void deleteCategoryImage(@PathVariable int categoryNo) {
 
         if (categoryDao.selectOne(categoryNo) == null) {
             throw new TargetNotFoundException("존재하지 않는 카테고리");
         }
-        categoryDao.deleteCategoryImage(categoryNo);
-    }
 
+        Integer attachmentNo =
+                categoryDao.selectCategoryImageAttachmentNo(categoryNo);
+
+        if (attachmentNo != null) {
+        	attachmentService.delete(attachmentNo);
+        }
+    }
 }
