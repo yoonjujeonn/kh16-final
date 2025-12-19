3736b5650e75e88f32f664c1c717a64d11f10234
hansoobin-11 <vcoart@naver.com>
2025-12-19 14:17:52 +0900

    리뷰

--- kh16fd/src/main/java/com/kh/fd/vo/ReviewListVO.java
+++ kh16fd/src/main/java/com/kh/fd/vo/ReviewListVO.java
@@ -5,9 +5,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+//평균 리뷰 조회
 @Data @AllArgsConstructor @Builder @NoArgsConstructor
 public class ReviewListVO {
 	public Long restaurantId;
 	public double restaurantAvgRating;
 	public int reviewCount;
 }
+
